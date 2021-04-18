179bbfd9bc7f975e37c17f9df943239eb68975ca
Joe Grassl <j03grassl@gmail.com>
2021-04-18 15:46:36 -0700

    Completed Assignment 3.

--- SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
@@ -5,10 +5,13 @@ namespace SecretSanta.Web.ViewModels
     public class GiftViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
         public string Url { get; set; } = "";
+        [Required]
         public int Priority { get; set; }
+        [Required]
         [Display(Name="User")]
         public int UserId { get; set; }
     }
--- SecretSanta/src/SecretSanta.Web/ViewModels/GroupViewModel.cs
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GroupViewModel.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SecretSanta.Web.ViewModels
 {
     public class GroupViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = "";
     }
 }

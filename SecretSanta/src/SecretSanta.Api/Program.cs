001640671e960f37d35dc644ee2d45099a6d3e14
Joe Grassl <j03grassl@gmail.com>
2021-06-07 16:55:28 -0700

    Finally got seeding to work!

--- SecretSanta/src/SecretSanta.Api/Program.cs
+++ SecretSanta/src/SecretSanta.Api/Program.cs
@@ -2,9 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using SecretSanta.Data;
-using DbContext = SecretSanta.Data.DbContext;
+using SecretSanta.Business;
 
 namespace SecretSanta.Api
 {
@@ -12,66 +10,9 @@ namespace SecretSanta.Api
     {
         public static void Main(string[] args)
         {
+            DataSeeder.Seed();
+
             CreateHostBuilder(args).Build().Run();
-            using DbContext dbContext = new DbContext();
-
-            // Seed data goes here.
-            // For some reason, this creates a database file of the same size as the one in test project,
-            // but this one has no data in it. How is that even possible?
-            var users = new[]
-            {
-                new User
-                {
-                    Id = 1,
-                    FirstName = "Inigo",
-                    LastName = "Montoya"
-                },
-                new User
-                {
-                    Id = 2,
-                    FirstName = "Princess",
-                    LastName = "Buttercup"
-                },
-                new User
-                {
-                    Id = 3,
-                    FirstName = "Prince",
-                    LastName = "Humperdink"
-                },
-                new User
-                {
-                    Id = 4,
-                    FirstName = "Count",
-                    LastName = "Rugen"
-                },
-                new User
-                {
-                    Id = 5,
-                    FirstName = "Miracle",
-                    LastName = "Max"
-                }
-            };
-            
-            var groups = new[]
-            {
-                new Group
-                {
-                    Id = 1,
-                    Name = "IntelliTect Christmas Party",
-                    Users = new List<User> { users[0], users[1], users[2], users[3], users[4] }
-                },
-                new Group
-                {
-                    Id = 2,
-                    Name = "Friends",
-                    Users = new List<User> { users[0], users[1], users[2], users[3], users[4] }
-                }
-            };
-
-            dbContext.AddRange(users);
-            dbContext.AddRange(groups);
-
-            dbContext.SaveChanges();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ SecretSanta/src/SecretSanta.Business/DataSeeder.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using SecretSanta.Data;
+
+namespace SecretSanta.Business
+{
+    public class DataSeeder
+    {
+        public static void Seed()
+        {
+            using DbContext dbContext = new DbContext();
+
+            // Seed data goes here.
+            var users = new[]
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Inigo",
+                    LastName = "Montoya"
+                },
+                new User
+                {
+                    Id = 2,
+                    FirstName = "Princess",
+                    LastName = "Buttercup"
+                },
+                new User
+                {
+                    Id = 3,
+                    FirstName = "Prince",
+                    LastName = "Humperdink"
+                },
+                new User
+                {
+                    Id = 4,
+                    FirstName = "Count",
+                    LastName = "Rugen"
+                },
+                new User
+                {
+                    Id = 5,
+                    FirstName = "Miracle",
+                    LastName = "Max"
+                }
+            };
+            
+            var groups = new[]
+            {
+                new Group
+                {
+                    Id = 1,
+                    Name = "IntelliTect Christmas Party",
+                    Users = new List<User> { users[0], users[1], users[2], users[3], users[4] }
+                },
+                new Group
+                {
+                    Id = 2,
+                    Name = "Friends",
+                    Users = new List<User> { users[0], users[1], users[2], users[3], users[4] }
+                }
+            };
+
+            dbContext.AddRange(users);
+            dbContext.AddRange(groups);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
